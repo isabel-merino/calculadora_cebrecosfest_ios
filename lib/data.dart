e2a290b80031c8f6aaa23fcbbf35f8914f528349
isabel-merino <lebasimc02@gmail.com>
2025-07-23 11:06:31 +0200

    Actualización precios

--- lib/data.dart
+++ lib/data.dart
@@ -17,16 +17,17 @@ class preciosBebidas{
   static Product marianito = Product("Marianito", 1.5, "assets/img/marianito.png");
   static Product vermouth = Product("Vermut", 2.5, "assets/img/vermut.png");
   static Product beer = Product("Caña", 1.5, "assets/img/canya.png");
-  static Product pint = Product("Pinta", 2.5, "assets/img/pinta.png");
+  static Product pint = Product("Pinta", 3, "assets/img/pinta.png");
   static Product radler = Product("Radler", 2.0, "assets/img/radler.png");
-  static Product kali = Product("Kalimotxo", 2.5, "assets/img/kali.png");
+  static Product kali = Product("Kalimotxo", 3.5, "assets/img/kali.png");
   static Product drink = Product("Cubata", 5.0, "assets/img/cubata.png");
-  static Product bigCupKali = Product("Katxi Kalimocho", 5.0, "assets/img/katxi.png");
+  static Product bigCupKali = Product("Katxi Kalimocho", 7.0, "assets/img/katxi.png");
+  static Product bigCupCerve = Product("Katxi Cerveza", 6.0, "assets/img/katxi.png");
   static Product bigCupDrink = Product("Katxi Cubata", 12.0, "assets/img/katxi.png");
   static Product shot = Product("Chupito", 1.5, "assets/img/shot.png");
   static Product frosty = Product("Granizado", 2.0, "assets/img/granizado.png");
 
-  static List<Product> bebidas = [water, beer, shot, drink, frosty, kali, bigCupDrink, bigCupKali, marianito, mosto, pint, radler, soda, vermouth, wine];
+  static List<Product> bebidas = [water, beer, shot, drink, frosty, kali, bigCupCerve, bigCupDrink, bigCupKali, marianito, mosto, pint, radler, soda, vermouth, wine];
 }
 
 class preciosComida{
